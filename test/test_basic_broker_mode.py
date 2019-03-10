4753177d8f47f4d6ce38b8b3cd780829d1ef631d
sudmat <dingjiesu@163.com>
2019-03-10 14:37:17 -0500

    temp modify

--- test/test_basic_broker_mode.py
+++ test/test_basic_broker_mode.py
@@ -13,17 +13,22 @@ zk_root = '/test_temp_dir'
 zookeeper = '127.0.0.1:2181'
 zk = KazooClient(hosts=zookeeper)
 
-broker1 = Broker({'mode': mode, 'port': 5555, 'zookeeper':zookeeper,
+broker = Broker({'mode': mode, 'port': 5555, 'zookeeper':zookeeper,
                   'logfile': 'log/temp_broker.log', 'broker_addr':'tcp://127.0.0.1:5555'},zk_root=zk_root)
-broker2 = Broker({'mode': mode, 'port': 5556, 'zookeeper':zookeeper,
-                  'logfile': 'log/temp_broker.log', 'broker_addr':'tcp://127.0.0.1:5556'},zk_root=zk_root)
-broker3 = Broker({'mode': mode, 'port': 5554, 'zookeeper':zookeeper,
-                  'logfile': 'log/temp_broker.log', 'broker_addr':'tcp://127.0.0.1:5554'},zk_root=zk_root)
 
-pub = Publisher(mode=mode, ip_address='tcp://127.0.0.1:5000',pub_name='Pub1',
-                zk_address=zookeeper, strength=0, logfile='log/temp_pub.log', zk_root=zk_root)
+pub1 = Publisher(mode=mode, ip_address='tcp://127.0.0.1:5000',pub_name='Pub1',
+                zk_address=zookeeper, logfile='log/temp_pub.log', zk_root=zk_root)
 
-sub = Subscriber(ip_self='tcp://127.0.0.1:5005', ip_zookeeper=zookeeper, name='Sub1',
+pub2 = Publisher(mode=mode, ip_address='tcp://127.0.0.1:5001',pub_name='Pub2',
+                zk_address=zookeeper, logfile='log/temp_pub.log', zk_root=zk_root)
+
+pub3 = Publisher(mode=mode, ip_address='tcp://127.0.0.1:5002',pub_name='Pub3',
+                zk_address=zookeeper, logfile='log/temp_pub.log', zk_root=zk_root)
+
+sub1 = Subscriber(ip_self='tcp://127.0.0.1:5010', ip_zookeeper=zookeeper, name='Sub1',
+                   comm_type=mode, logfile='log/temp_sub.log', zk_root=zk_root)
+
+sub2 = Subscriber(ip_self='tcp://127.0.0.1:5011', ip_zookeeper=zookeeper, name='Sub2',
                    comm_type=mode, logfile='log/temp_sub.log', zk_root=zk_root)
 
 def test_start():
@@ -32,54 +37,74 @@ def test_start():
         zk.create(zk_root)
     except:
         pass
-
-    thread1 = threading.Thread(target=broker1.start)
+    thread1 = threading.Thread(target=broker.start)
     thread1.start()
     time.sleep(1)
 
-    thread2 = threading.Thread(target=broker2.start)
-    thread2.start()
-    time.sleep(1)
-
 def test_publisher_register():
-    res = pub.register('hello')
-    assert res == 0
+    res1 = pub1.register([{'topic': 'hello', 'history': 3}])
+    res2 = pub1.register([{'topic': 'hello', 'history': 5}])
+    pubs = zk.get_children('%s/Topic/hello/Pub'%zk_root)
+
+    assert res1 == 0 and res2 == 0
+    assert len(pubs) == 2
 
 def test_subscriber_register():
-    res = sub.register('hello')
-    assert res == 0
+    res1 = sub1.register([{'topic': 'hello', 'history': 4}])
+    res2 = sub2.register([{'topic': 'hello', 'history': 6}])
+    subs = zk.get_children('%s/Topic/hello/Sub' % zk_root)
 
-def test_publish_and_receive():
+    assert res1 == 0 and res2 == 0
+    assert len(subs) == 2
+
+def test_publish_and_receive1():
 
     def publish():
         for i in range(3):
-            pub.publish('hello', 'world')
+            pub1.publish('hello', 'world')
+            pub2.publish('hello', 'world')
 
     pub_thread = threading.Thread(target=publish)
     pub_thread.start()
-    msg = sub.receive()
-    assert msg['topic'] == 'hello'
-    assert msg['value'] == 'world'
+
+    sub_thread = threading.Thread(target=sub1.receive)
+    sub_thread.start()
+    sub_thread.join(timeout=1)
+
+    assert sub_thread.is_alive() # no msg received, so the sub_thread keeps waiting
     pub_thread.join()
 
-def test_pubsub_after_broker_leave():
-    broker1.stop()
-    broker2.stop()
-    thread = threading.Thread(target=broker3.start)
-    thread.start()
-    time.sleep(1)
-    test_publish_and_receive()
 
-def test_pub_exit():
-    pub.drop_system()
+def test_publish_and_receive2():
+    def publish():
+        for i in range(3):
+            pub2.publish('hello', 'world')
 
-def test_sub_exit():
-    sub.exit()
+    pub1.drop_system()
+    time.sleep(3)
 
-def test_broker_exit():
-    broker1.stop()
-    broker2.stop()
-    broker3.stop()
+    pub_thread = threading.Thread(target=publish)
+    pub_thread.start()
+
+    msg = sub1.receive()
+    assert msg['topic'] == 'hello' and msg['value'] == 'world'
+    pub_thread.join()
+
+
+def test_publish_and_receive3():
+    def publish():
+        for i in range(3):
+            pub2.publish('hello', 'world')
+
+    pub_thread = threading.Thread(target=publish)
+    pub_thread.start()
+
+    sub_thread = threading.Thread(target=sub2.receive)
+    sub_thread.start()
+    sub_thread.join(timeout=1)
+
+    assert sub_thread.is_alive()  # no msg received, so the sub_thread keeps waiting
+    pub_thread.join()
 
 def test_clean():
     try:
